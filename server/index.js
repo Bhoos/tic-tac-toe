325d50237d7ac29a5393e5d336d4fbc1abaf1cf5
Ranjan Shrestha <ranjan@sharingapples.com>
2022-05-06 23:18:46 +0545

    Add basic random bot server

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url) res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+  if (req.method === 'OPTIONS') {
+    res.end();
+    return;
+  }
+
+  let payload = '';
+  req.on('data', chunk => {
+    payload += chunk;
+  });
+
+  req.on('end', () => {
+    let result = { error: 'Unknown request' };
+    if (req.url.endsWith('play')) {
+      console.log('Got play request');
+      result = play(JSON.parse(payload));
+    } else {
+      console.log('Got unknown request');
+    }
+
+    res.write(JSON.stringify(result));
+    res.end();
+  });
+});
+
+function play(payload) {
+  const { turn, state } = payload;
+  let row = -1;
+  let col = -1;
+  let tries = 0;
+  do {
+    row = Math.floor(Math.random() * 3);
+    col = Math.floor(Math.random() * 3);
+    tries += 1;
+  } while (state[row][col] !== '' && tries < 100)
+  return { row, col };
+}
+
+server.listen(7777);
